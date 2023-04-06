09e83821790bf30c5cccf634079843f246eae8cc
Class Account <eecs151-abz@c111-16.eecs.berkeley.edu>
2023-04-06 00:08:35 -0700

    branch comparator (might need optimization)

---
+++ hardware/src/riscv_core/branch_comp.v
@@ -0,0 +1,28 @@
+module branch_comp {
+    parameter N = 32
+}
+(
+    input [N-1:0] br_data0, br_data1,
+    input BrUn,
+    output BrEq, BrLt
+);
+
+reg BrLt;
+
+wire b0 = br_data0[N-1];
+wire b1 = br_data1[N-1] 
+
+always @(*) begin
+    if (BrUn || ~(b0 || b1))        // unsigned or 2 positive 2's complement
+        BrLt = br_data0 < br_data1;
+    else if (b0 && b1)              // 2 negative 2's complement
+        BrLt = br_data1 < br_data0;
+    else if (b0)                    // br_data0 is negative
+        BrLt = 1'b1;
+    else if (b1)                    // br_data1 is negative
+        BrLt = 1'b0;
+end
+
+assign BrEq = br_data0 == br_data1; // equal regardless of sign system
+
+endmodule
