64db0f430ad7738b64d87a3db2424f7aedba0eca
Class Account <eecs151-adi@c111-15.eecs.berkeley.edu>
2023-04-07 22:46:31 -0700

    avr: control unit v2

--- hardware/src/riscv_core/control_unit.v
+++ hardware/src/riscv_core/control_unit.v
@@ -1,25 +1,10 @@
 module control_unit(
-    input [15:0] hex,
-    input [5:0] instr_code,
-
-    output RegWEn,
-    output [2:0] ImmSel,
-    output BrLUn, // used for load unsigned and branch unsigned
-    output ASel,
-    output BSel,
-    output [3:0] ALUSel,
-    output MemRW,
-    output [1:0] WBSel,
-    output [1:0] PCSel
+    input [5:0] dec_instr_code,
+    output [15:0] hex_instr_code
 );
-    assign RegWEn = hex[0];
-    assign ImmSel = hex[3:1];
-    assign BrLUn = hex[4];
-    assign ASel = hex[5];
-    assign BSel = hex[6];
-    assign ALUSel = hex[10:7];
-    assign MemRW = hex[11];
-    assign WBSel = hex[13:12];
-
-
+    localparam DATA_WIDTH = 16;
+    localparam ADDR_WIDTH = 6;
+    ASYNC_ROM #(.DWIDTH(DATA_WIDTH), .AWIDTH(ADDR_WIDTH), .MIF_HEX("ROM.mif")) 
+    async_rom (.addr(dec_instr_code),
+               .q(hex_instr_code));
 endmodule
