f098099955682f0703694e7db9c9f85e1dc275cd
Class Account <eecs151-abz@c111-2.eecs.berkeley.edu>
2023-04-07 23:28:51 -0700

    npc select unit done

---
+++ hardware/src/riscv_core/pc_sel_unit.v
@@ -0,0 +1,19 @@
+module pc_sel_unit(
+    input [5:0] dec_instr_code,
+    input BrEq, BrLt,
+    output PCSel
+);
+    `include "instr.vh"
+    wire br_jalr;
+    assign br_jalr =
+	(dec_instr_code == BEQ && BrEq) ||
+	(dec_instr_code == BNE && ~BrEq) ||
+	((dec_instr_code == BLT || dec_instr_code == BLTU) && BrLt) ||
+	((dec_instr_code == BGE || dec_instr_code == BGEU) && ~BrLt) ||
+	(dec_instr_code == JALR);
+
+   assign PCSel =
+	(dec_instr_code == JAL) ? 2'b01 :
+	(br_jalr ? 2'b10 : 2'b00);
+
+endmodule
