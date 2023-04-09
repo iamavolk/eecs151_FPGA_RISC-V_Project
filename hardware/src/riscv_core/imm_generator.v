bf72da9c9cfea61ea420b19c19a3f5b38474cf43
Class Account <eecs151-abz@c111-2.eecs.berkeley.edu>
2023-04-08 23:05:14 -0700

    ns: imm  modified

--- hardware/src/riscv_core/imm_generator.v
+++ hardware/src/riscv_core/imm_generator.v
@@ -8,16 +8,33 @@ module imm_generator #(
     output [N-1:0] imm
 );
 
-wire [N-1:0] s_type = {instr[31:25], instr[11:7]};
-wire [N-1:0] i_type = instr[21:10];
-wire [N-1:0] u_type = instr[31:12] << 12;
-wire [N-1:0] j_type = instr[31:12];
+wire [N-1:0] i_in = instr[21:10];
+wire [N-1:0] b_in = instr[31:12];
 
+wire [N-1:0] s_type =
+	{{N-12{1'b0}},
+	instr[31:25],
+	instr[11:7]};
 
-mux4 imm_gen(.in0(s_type),
-        .in1(i_type),
-        .in2(u_type),
-        .in3(j_type),
+wire [N-1:0] i_type =
+	{{N-12{1'b0}},
+	instr[31:20]};
+
+wire [N-1:0] u_type =
+	instr[31:12] << 12;
+
+wire [N-1:0] b_type =
+	{{N-13{1'b0}},
+	instr[31],
+	instr[7],
+	instr[30:25],
+	instr[11:8],
+	1'b0};
+
+mux4 imm_gen(.in0(i_type),
+        .in1(s_type),
+        .in2(b_type),
+        .in3(u_type),
         .sel(imm_sel),
         .out(imm)
 );
