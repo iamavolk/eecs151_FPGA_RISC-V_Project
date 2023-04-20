e2364fe776da3fc9283a916d67bbfcebdd37dc8e
avr <alexfromshermanoaks@berkeley.edu>
2023-04-19 23:37:56 -0700

    avr: ROM.mif given absolute path

--- hardware/src/riscv_core/control_unit.v
+++ hardware/src/riscv_core/control_unit.v
@@ -4,7 +4,8 @@ module control_unit(
 );
     localparam DATA_WIDTH = 16;
     localparam ADDR_WIDTH = 6;
-    ASYNC_ROM #(.DWIDTH(DATA_WIDTH), .AWIDTH(ADDR_WIDTH), .MIF_HEX("../../hardware/src/riscv_core/ROM.mif")) 
+    //ASYNC_ROM #(.DWIDTH(DATA_WIDTH), .AWIDTH(ADDR_WIDTH), .MIF_HEX("../../hardware/src/riscv_core/ROM.mif")) 
+    ASYNC_ROM #(.DWIDTH(DATA_WIDTH), .AWIDTH(ADDR_WIDTH), .MIF_HEX("/home/cc/eecs151/sp23/class/eecs151-adi/fpga_project_sp23-bear_the_risc/hardware/src/riscv_core/ROM.mif")) 
     async_rom (.addr(dec_instr_code),
                .q(hex_instr_code));
 endmodule
